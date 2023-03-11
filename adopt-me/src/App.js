a32d7e42c198c657e6d28cd65dcccec6611522ba
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-03-11 07:22:06 -0600

    Prettier config vscode and module

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,22 +1,26 @@
-const Pet = ( props ) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, props.name),
-            React.createElement("h2", {}, props.animal),
-            React.createElement("h3", {}, props.breed)
-        ]
-    );
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
 const App = () => {
-    return React.createElement("div",{}, [
-        React.createElement("h1", {}, "Adopt me!"),
-        React.createElement(Pet, {name: 'Luna', animal: 'Dog', breed: 'Havanesse'}),
-        React.createElement(Pet, {name: 'Gatis', animal: 'Cat', breed: 'Street'}),
-        React.createElement(Pet, {name: 'Ratón', animal: 'Mouse', breed: 'House'}),
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanesse",
+    }),
+    React.createElement(Pet, { name: "Gatis", animal: "Cat", breed: "Street" }),
+    React.createElement(Pet, {
+      name: "Ratón",
+      animal: "Mouse",
+      breed: "House",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
