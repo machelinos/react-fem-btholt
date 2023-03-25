0a48b87999fcb24e0eeb93143de763c0b600d915
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-03-25 16:12:53 -0600

    04 Hooks intro: useState

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanesse" />
-      <Pet name="Gatis" animal="Cat" breed="Street" />
-      <Pet name="Ratón" animal="Mouse" breed="House" />
+      <SearchParams />
     </div>
   );
 };
---
+++ adopt-me/src/SearchParams.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={!breeds.length}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
