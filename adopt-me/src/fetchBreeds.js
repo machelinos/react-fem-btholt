0b1bef2dd0f0a5e80930d1c3d687787ac3a2d069
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-03-31 01:15:30 -0600

    09 react query

---
+++ adopt-me/src/fetchBreeds.js
@@ -0,0 +1,14 @@
+const fetchBreeds = async ({ queryKey }) => {
+  const animal = queryKey[1];
+  if (!animal) return [];
+
+  const resApi = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!resApi.ok) throw new Error("fetching breeds failed");
+
+  return resApi.json();
+};
+
+export default fetchBreeds;
--- adopt-me/src/useBreedList.js
+++ adopt-me/src/useBreedList.js
@@ -1,33 +1,7 @@
-import { useEffect, useState } from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreeds from "./fetchBreeds";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
-
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList(animal);
-    }
-
-    async function requestBreedList(animal) {
-      setBreedList([]);
-      setStatus("loading");
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  const results = useQuery(["breeds", animal], fetchBreeds);
+  return [results?.data?.breeds ?? [], results.status];
 }
