e1c4a511f44d0b70044e6219b09f2692b31af196
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-05-18 01:05:31 -0600

    Intermediate - Typescript setup

--- adopt-me/src/Modal.tsx
+++ adopt-me/src/Modal.tsx
@@ -1,18 +1,25 @@
-import { useEffect, useRef } from "react";
+import { MutableRefObject, ReactElement, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal = ({ children }: { children: ReactElement }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
+
     modalRoot.appendChild(elRef.current);
 
     return () => {
-      return modalRoot.removeChild(elRef.current);
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
     };
   }, []);
 
